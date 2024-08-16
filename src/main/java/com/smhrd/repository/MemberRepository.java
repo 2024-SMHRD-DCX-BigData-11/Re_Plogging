52fe7aa4b8809009167512d018754c160ac87972
yonggu2 <thwjd1342@naver.com>
2024-08-16 17:24:50 +0900

    2024.08.16_배소정_@Parmam, @RequestParam 추가

--- src/main/java/com/smhrd/repository/MemberRepository.java
+++ src/main/java/com/smhrd/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.smhrd.entity.Member;
 
@@ -20,7 +21,8 @@ public interface MemberRepository extends JpaRepository<Member, Integer> {
 		    and u.userPw = :userPw
 		    """)
 	
-		public Member findByUserIdAndUserPw(String userId, String userPw);
+		public Member findByUserIdAndUserPw(@Param("userId") String userId, 
+	            @Param("userPw") String userPw);
 	
 	
 	@Query("""
