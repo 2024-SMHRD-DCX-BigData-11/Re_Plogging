efeadc34d3bab6ba67f6853b55cbc0067a55b02c
sone6ee <opo226@naver.com>
2024-08-20 11:23:35 +0900

    2024.08.20_봉소희_분리배출 모바일.ver 작업, header.js 스크롤 효과 수정

--- src/main/resources/static/assets/js/header.js
+++ src/main/resources/static/assets/js/header.js
@@ -5,7 +5,10 @@ window.addEventListener('scroll', function() {
     const header = document.querySelector('.header');
     const currentScrollY = window.scrollY;
 
-    if (currentScrollY > lastScrollY) {
+    if (currentScrollY === 0) {
+        // 최상단에서는 항상 헤더를 보이게 하기
+        header.classList.remove('hidden');
+    } else if (currentScrollY > lastScrollY) {
         // 스크롤이 아래로 이동 중이면 헤더를 숨기기
         header.classList.add('hidden');
     } else {
