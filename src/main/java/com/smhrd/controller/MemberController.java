432d22f3187b0b5c29909adcd9b8a429aa5a0daa
qwer010310 <krseokju@gmail.com>
2024-08-22 11:27:36 +0900

    2024-08-22_최석주_멤버 컨트롤러 주석 제거

--- src/main/java/com/smhrd/controller/MemberController.java
+++ src/main/java/com/smhrd/controller/MemberController.java
@@ -94,44 +94,7 @@ public class MemberController {
 		return "test";
 	}
 
-	/*
-	 * @RequestMapping("/join") public String join(Member member) { // 사용자가 입력한
-	 * 비밀번호를 가져옴 String joinPw = member.getUserPw();
-	 * 
-	 * // 비밀번호를 SHA-512 방식으로 암호화 String joinEncryptedPw =
-	 * DigestUtils.sha512Hex(joinPw);
-	 * 
-	 * // 암호화된 비밀번호를 Member 객체에 설정 member.setUserPw(joinEncryptedPw);
-	 * 
-	 * repo.save(member);
-	 * 
-	 * // 저장 후 메인 페이지로 리다이렉트 return "redirect:/main"; }
-	 */
-
-	/*
-	 * @RequestMapping("/join") public String join(Member member) { // 0. 사전 준비 :
-	 * Dependency 추가 > DB 연결 설정까지 // Table, DTO, Mapper와 같은 파일들이 있어야 함
-	 * 
-	 * System.out.println("회원가입 직전 비밀번호: " + member.getUserPw()); // 1. 데이터 수집 // 2.
-	 * 기능 실행 > 사용자가 입력한 정보를 DB에 저장 ! repo.save(member);
-	 * 
-	 * // 3. View 선택 return "redirect:/main"; }
-	 */
-
-//		@RequestMapping("/nickCheck")
-//		public int  nickCheck(@RequestParam String userNick) {
-//			
-//			Member member = repo.findByUserNick(userNick);
-//			
-//			if(member == null) {
-//				//중복 안됨
-//				return 0;
-//			}
-//			
-//			else {
-//				return 1;
-//			}	
-//		}
+	
 
 	// Get, Post Mapping 어노테이션을 이용해 URL 요청이 들어왔을 때, 요청 방식에 따라 다른 기능이 실행되게 할 수 있음.
 	// 즉, URL 매핑이 겹쳐도 사용할 수 있게 됨.
