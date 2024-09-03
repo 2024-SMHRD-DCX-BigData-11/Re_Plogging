710a95b68d42237f618b6752c8c496a32096312c
smhrd1 <smhrd1@smhrd>
2024-09-03 09:55:56 +0900

    2024.09.03 회원가입 소생

--- src/main/java/com/smhrd/restController/MemberRestController.java
+++ src/main/java/com/smhrd/restController/MemberRestController.java
@@ -1,15 +1,22 @@
 package com.smhrd.restController;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 import org.apache.commons.codec.digest.DigestUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.smhrd.entity.CommonDomain;
 import com.smhrd.entity.Member;
+import com.smhrd.entity.Mileage;
 import com.smhrd.repository.MemberRepository;
+import com.smhrd.repository.MileageRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -20,6 +27,79 @@ public class MemberRestController {
 
     @Autowired
     private MemberRepository repo;
+    
+    @Autowired
+    private MileageRepository mileageRepo;
+    
+    @RequestMapping("/check")
+    public String check(int userIdx) {
+        Optional<Member> member = repo.findById(userIdx);
+        boolean check = member.isEmpty();
+        return check + "";
+    }
+    
+    @RequestMapping("/testA")
+    public CommonDomain testA(int userIdx) {
+        CommonDomain response = new CommonDomain();
+        Map<String, Object> resultMap = new HashMap<>();
+        Optional<Member> member = repo.findById(userIdx);
+        if (member.isPresent()) {
+            resultMap.put("id", member.get().getUserId());
+            resultMap.put("nickname", member.get().getUserNick());
+            response.setResult(resultMap);
+        }
+        return response;
+    }
+    
+    @RequestMapping(value = "/join", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonDomain userRegist(MultipartHttpServletRequest request) {
+        CommonDomain response = new CommonDomain();
+        
+        String id = request.getParameter("userId").toString();
+        String pass = request.getParameter("userPw").toString();
+        String phone = request.getParameter("userPhone").toString();
+        String nick = request.getParameter("userNick").toString();
+        
+        int resultCode = 0;
+        
+        Member member = repo.findByEmail(id);
+        if (member == null) {
+            member = repo.findByPhone(phone);
+            if (member == null) {
+                member = repo.findByUserNick(nick);
+                if (member == null) {
+                    member = new Member();
+                    Mileage mileage = new Mileage();
+                    
+                    member.setUserId(id);
+                    String joinEncryptedPw = DigestUtils.sha512Hex(pass);
+                    member.setUserPw(joinEncryptedPw);
+                    member.setUserPhone(phone);
+                    member.setUserNick(nick);
+                    
+                    mileage.setMlAmount(300);
+                    mileage.setUser(member);
+                    mileage.setMlType("회원가입");
+                    mileage.setMlLog("적립"); // mlLog 필드에 값 설정
+                    
+                    repo.save(member);
+                    mileageRepo.save(mileage);
+                    
+                    resultCode = member.getUserIdx() != 0 ? 200 : -100;
+                } else {
+                    resultCode = -300; // 닉네임 중복
+                }
+            } else {
+                resultCode = -400; // 전화번호 중복
+            }
+        } else {
+            resultCode = -500; // 이메일 중복
+        }
+        
+        response.setCode(resultCode);
+        return response;
+    }
 
     // 닉네임 중복 체크 및 변경
     @RequestMapping(value = "/updateNickname", method = RequestMethod.POST)
